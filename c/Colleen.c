3c1167a609ddd8735dc96eb3df420ac9ab429a15
Pradene <liam.pradene@gmail.com>
2025-04-09 17:02:45 +0200

    Adding another function inside source code of Colleen

--- c/Colleen.c
+++ c/Colleen.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#define CODE "#include <stdio.h>%c#define CODE %c%s%c%c%cint main() {%c  printf(CODE, 10, 34, CODE, 34, 10, 10, 10, 10, 10, 10, 10);%c%c  return (0);%c}%c"
+#define CODE "#include <stdio.h>%c#define CODE %c%s%c%c%cint run() {%c  printf(CODE, 10, 34, CODE, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c%c  return (0);%c}%c%cint main() {%c  return (run());%c}%c"
 
-int main() {
-  printf(CODE, 10, 34, CODE, 34, 10, 10, 10, 10, 10, 10, 10);
+int run() {
+  printf(CODE, 10, 34, CODE, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 
   return (0);
+}
+
+int main() {
+  return (run());
 }
