e167bb12111c20a03f1b68377649d6a5a3b37569
Pradene <liam.pradene@gmail.com>
2025-04-09 17:09:32 +0200

    Adding comments

--- c/Colleen.c
+++ c/Colleen.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#define CODE "#include <stdio.h>%c#define CODE %c%s%c%c%cint run() {%c  printf(CODE, 10, 34, CODE, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c%c  return (0);%c}%c%cint main() {%c  return (run());%c}%c"
+#define CODE "#include <stdio.h>%c#define CODE %c%s%c%c%c// Inception%cint run() {%c  printf(CODE, 10, 34, CODE, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c%c  return (0);%c}%c%cint main() {%c  // Print source code inside stdout%c  return (run());%c}%c"
 
+// Inception
 int run() {
-  printf(CODE, 10, 34, CODE, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+  printf(CODE, 10, 34, CODE, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 
   return (0);
 }
 
 int main() {
+  // Print source code inside stdout
   return (run());
 }
