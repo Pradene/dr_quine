5bac5ac22d9e35cac8fa822e35a1b77be0337867
Pradene <liam.pradene@gmail.com>
2025-04-10 22:43:06 +0200

    File recursive creation

--- c/Sully.c
+++ c/Sully.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define CODE "#include <stdio.h>%c#include <stdlib.h>%c#define CODE %c%s%c%c%cint main() {%c  int i = %c;%c  if (i < 0) return (1);%c  char new_filename[256] = {0};%c  sprintf(new_filename, %cSully_%cc.c%c, (i - 1) + 48);%c  FILE *file = fopen(new_filename, %cw%c);%c  if (file == 0) return (1);%c  fprintf(file, CODE, 10, 10, 34, CODE, 34, 10, 10, 10, (i - 1) + 48, 10, 10, 10, 34, 37, 34, 10, 34, 34, 10, 10, 10, 10, 10, 34, 37, 37, 34, 10, 10, 10, 10);%c  fclose(file);%c  char command[512] = {0};%c  sprintf(command, %ccc -Wall -Wextra -Werror %cs -o Sully_%cc%c, __FILE__, (i - 1) + 48);%c  system(command);%c  return (0);%c}%c"
+#include <string.h>
+#define CODE "#include <stdio.h>%2$c#include <stdlib.h>%2$c#include <string.h>%2$c#define CODE %3$c%1$s%3$c%2$c%2$cint main() {%2$c  int i = %4$d;%2$c  if (i <= 0) return (1);%2$c  char current_filename[256] = {0};%2$c  sprintf(current_filename, %3$cSully_%5$cd.c%3$c, i);%2$c  if (strcmp(current_filename, __FILE__) == 0) i--;%2$c  char new_exec[256] = {0};%2$c  sprintf(new_exec, %3$cSully_%5$cd%3$c, i);%2$c  char new_filename[256] = {0};%2$c  sprintf(new_filename, %3$cSully_%5$cd.c%3$c, i);%2$c  FILE *file = fopen(new_filename, %3$cw%3$c);%2$c  if (file == 0) return (1);%2$c  fprintf(file, CODE, CODE, 10, 34, i, 37);%2$c  fclose(file);%2$c  char command_compile[1024] = {0};%2$c  sprintf(command_compile, %3$ccc -Wall -Wextra -Werror %5$cs -o %5$cs%3$c, new_filename, new_exec);%2$c  system(command_compile);%2$c  char command_exec[512] = {0};%2$c  sprintf(command_exec, %3$c./%5$cs%3$c, new_exec);%2$c  system(command_exec);%2$c%2$c  return (0);%2$c}%2$c"
 
 int main() {
   int i = 5;
-  if (i < 0) return (1);
+  if (i <= 0) return (1);
+  char current_filename[256] = {0};
+  sprintf(current_filename, "Sully_%d.c", i);
+  if (strcmp(current_filename, __FILE__) == 0) i--;
+  char new_exec[256] = {0};
+  sprintf(new_exec, "Sully_%d", i);
   char new_filename[256] = {0};
-  sprintf(new_filename, "Sully_%c.c", (i - 1) + 48);
+  sprintf(new_filename, "Sully_%d.c", i);
   FILE *file = fopen(new_filename, "w");
   if (file == 0) return (1);
-  fprintf(file, CODE, 10, 10, 34, CODE, 34, 10, 10, 10, (i - 1) + 48, 10, 10, 10, 34, 37, 34, 10, 34, 34, 10, 10, 10, 10, 10, 34, 37, 37, 34, 10, 10, 10, 10);
+  fprintf(file, CODE, CODE, 10, 34, i, 37);
   fclose(file);
-  char command[512] = {0};
-  sprintf(command, "cc -Wall -Wextra -Werror %s -o Sully_%c", __FILE__, (i - 1) + 48);
-  system(command);
+  char command_compile[1024] = {0};
+  sprintf(command_compile, "cc -Wall -Wextra -Werror %s -o %s", new_filename, new_exec);
+  system(command_compile);
+  char command_exec[512] = {0};
+  sprintf(command_exec, "./%s", new_exec);
+  system(command_exec);
+
   return (0);
 }
